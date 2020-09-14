da316f7137cbfc2b982f2694a2c7162b39f2a096
jesssubin <jess.s.moon@gmail.com>
2020-09-14 20:20:04 +0000

    tail function

---
+++ tail.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    return `${actual} is equal to ${expected}`;
+  } else if (actual !== expected) {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+    return `${actual} is not equal to ${expected}`;
+  }
+};
+
+const tail = function(arr) {
+  let newArr = [];
+  for (let i = 1; i < arr.length; i++) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+assertEqual(result, ["Lighthouse", "Labs"]);
