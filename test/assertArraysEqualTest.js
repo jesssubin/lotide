50a30df789a6517f863599d6f12a47bc8b823cb1
jesssubin <jess.s.moon@gmail.com>
2020-09-21 19:02:20 +0000

    refactored

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays'); 
+const assertArraysEqual = require('../assertArraysEqual'); 
+
+let a1 = ['cat', 'dog', 'fish'];
+let a2 = ['cat', 'dog', 'fish'];
+
+assertArraysEqual(a1, a2); 
