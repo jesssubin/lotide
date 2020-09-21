77f5c002639aaf9187f31859335122fd181fc6c6
jesssubin <jess.s.moon@gmail.com>
2020-09-21 19:51:26 +0000

    refactored using mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,25 @@
+const assert = require('chai').assert;
 const tail = require('../tail'); 
 const assertEqual = require('../assertEqual');
 
 //Test Code
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-assertEqual(result, ["Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// assertEqual(result, ["Lighthouse", "Labs"]);
+
+describe("#tail", () => {
+  it("returns 2 for result.length", () => {
+    assert.strictEqual(result.length, 2); 
+  }); 
+  it("returns Lighthouse for result[0]", () => {
+    assert.strictEqual(result[0], "Lighthouse");
+  }); 
+  it("returns Labs for result[1]", () => {
+    assert.strictEqual(result[1], "Labs");
+  }); 
+  it("returns Lighthouse, Labs for result", () => {
+    assert.strictEqual(result, ["Lighthouse", "Labs"]);
+  }); 
+})
