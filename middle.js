a3a35b10e01ee2c5ea94bc8dcf15a0cfd90bb391
jesssubin <jess.s.moon@gmail.com>
2020-09-21 19:08:31 +0000

    refactroed

--- middle.js
+++ middle.js
@@ -1,22 +1,5 @@
-const eqArrays = function (arr1, arr2) {
-  let result = true;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      result = true;
-    } else {
-      result = false;
-    }
-    return result;
-  }
-}
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`⭕️⭕️⭕️${arr1} is equal to ${arr2}`);
-  } else {
-    console.log(`❌❌❌${arr1} is NOT equal to ${arr2}`);
-  }
-}; 
+const assertArraysEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
 const middle = function (arr) {
   let arrLength = arr.length;
@@ -33,10 +16,6 @@ const middle = function (arr) {
   return middleArr; 
 }
 
-//TEST CODES
-console.log(middle([1])) // => []
-console.log(middle([1, 2]))// => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5]))// => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+
+
+module.exports = middle; 
