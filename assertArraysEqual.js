241f82753a0cb7ce8b4cd3e5de6109520cf3d4ce
jesssubin <jess.s.moon@gmail.com>
2020-09-21 19:03:02 +0000

    refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,38 +1,16 @@
-const assertEqual = function (actual, expected) {
-
-  if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
-    return `${actual} is equal to ${expected}`;
-  } else if (actual !== expected) {
-    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
-    return `${actual} is not equal to ${expected}`;
-  }
-};
-
-const eqArrays = function (arr1, arr2) {
-  let result = true;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      result = true;
-    } else {
-      result = false;
-    }
-    return result;
-  }
-} 
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function (arr1, arr2) {
   if (eqArrays(arr1, arr2)){
-    console.log(`⭕️⭕️⭕️${arr1} is equal to ${arr2}`); 
+    console.log(`💚💚💚 ${arr1} is equal to ${arr2}`); 
   } else {
-    console.log(`❌❌❌${arr1} is NOT equal to ${arr2}`);
+    console.log(`💔💔💔 ${arr1} is NOT equal to ${arr2}`);
   }
 };
 
-let a1 = ['cat', 'dog', 'fish']; 
-let a2 = ['cat', 'dog', 'fish']; 
 
-assertArraysEqual(a1, a2); 
 
+module.exports = assertArraysEqual; 
 
 
